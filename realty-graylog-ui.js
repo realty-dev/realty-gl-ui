e32cccb4a32e271806d4d7dfcfb392c2659e7c58
Alex Gibson <35852411+realty-dev@users.noreply.github.com>
2019-04-08 13:02:35 -0400

    Update realty-graylog-ui.js

--- realty-graylog-ui.js
+++ realty-graylog-ui.js
@@ -1,1 +1,14 @@
-GM_addStyle('.message-wrapper { color: black; }');
+// ==UserScript==
+// @name         Realty.com Graylog UI
+// @namespace    http://dashboard.realty.com:9000
+// @version      0.12
+// @updateURL    https://raw.githubusercontent.com/realty-dev/realty-gl-ui/master/realty-graylog-ui.js
+// @description  try to take over the world!
+// @author       Alex Gibson
+// @match        http://dashboard.realty.com:9000/*
+// @grant        GM_addStyle
+// @grant        GM_log
+// ==/UserScript==
+
+
+GM_addStyle('.message-wrapper { color: white; }');
